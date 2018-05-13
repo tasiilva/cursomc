1f8af8a11b3a7b517f7f91ed9795455b27aae09f
Tiago Silva <tasilva@outlook.com>
2018-05-12 22:14:49 -0300

    Cliente, TipoCliente, telefone e endereços

--- src/main/java/com/nelioalves/cursomc/domain/Cidade.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cidade.java
@@ -11,7 +11,7 @@ import javax.persistence.ManyToOne;
 
 @Entity
 public class Cidade implements Serializable{
-	private static final long serialVersionUID = 1L;	
+	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
